198b9601ea9c10b8181081baaaffa6cb965dac0a
Saul Lee <saul.s.lee@gmail.com>
2023-02-14 18:04:59 -0500

    add new queries to select data and update table

--- airflow/dags/utils/postgres_table.py
+++ airflow/dags/utils/postgres_table.py
@@ -48,11 +48,20 @@ class column_def():
 
 class postgres_table():
 
-    def __init__(self, schema_name: str, table_name: str, column_list: list):
+    def __init__(
+        self,
+        schema_name: str,
+        table_name: str,
+        column_list: list,
+        checkpoint_id_col: str = "event_id",
+        checkpoint_timestamp_col: str = "event_timestamp",
+    ):
         self.schema_name = schema_name
         self.table_name = table_name
         self.column_list = column_list
         self.full_table_name = f"{self.schema_name}.{self.table_name}"
+        self.checkpoint_id_col = checkpoint_id_col
+        self.checkpoint_timestamp_col = checkpoint_timestamp_col
 
     def query_create_schema(self, exists_condition=True):
         """
@@ -97,7 +106,8 @@ class postgres_table():
 
         return f"""
         CREATE TABLE {exists_str} {self.full_table_name} (
-            {query_column_str}
+            {query_column_str},
+            etl_time TIMESTAMPTZ DEFAULT (NOW() AT TIME ZONE 'utc')
         );
         """
 
@@ -124,6 +134,15 @@ class postgres_table():
         SELECT COUNT(*) FROM {self.full_table_name}
         """
 
+    def query_select_star(self, limit=100):
+        """
+        Generate query to select table
+        """
+
+        return f"""
+        SELECT * FROM {self.full_table_name} LIMIT {limit}
+        """
+
     def query_insert_from_list(self, data_list):
         """
         Generate query to insert data into a table in the database
@@ -136,12 +155,72 @@ class postgres_table():
         )
 
         value_str = ",\n".join(
-            [str(data_item) for data_item in data_list]
+            [f"{data_item}" for data_item in data_list]
         )
+
+        # replace 'NULL' with just NULL
+        value_str = value_str.replace("\'NULL\'", "NULL")
 
         return f"""
         INSERT INTO {self.full_table_name} ({column_str})
         VALUES
             {value_str}
+        ;
+        """
+
+    def query_load_updated_data(self, source_table):
+        """
+        Generate query to insert data into a table in the database
+
+        data_list is expected to be a list of records where each
+        item is a tuple of a row to be inserted
+        """
+        column_str = ",".join(
+            [column_item.get_column_name() for column_item in self.column_list]
+        )
+        column_str += ",etl_time"
+
+        temp_column_str = ",".join(
+            [f"temp.{column_item.get_column_name()}" for column_item in self.column_list]
+        )
+        temp_column_str += ",etl_time"
+
+        return f"""
+        INSERT INTO {self.full_table_name} ({column_str})
+
+        WITH prod_date_tbl AS (
+            SELECT
+                {self.checkpoint_id_col},
+                MAX({self.checkpoint_timestamp_col}) AS max_timestamp
+            FROM
+                {self.full_table_name}
+            GROUP BY
+                {self.checkpoint_id_col}
+        ),
+        labeled_new_tbl AS (
+            SELECT
+                {temp_column_str},
+                CASE
+                    WHEN prod.max_timestamp IS NULL
+                        THEN 1
+                    WHEN temp.{self.checkpoint_timestamp_col} > prod.max_timestamp
+                        THEN 1
+                    ELSE
+                        0
+                END AS is_new
+            FROM
+                {source_table.full_table_name} AS temp
+            LEFT JOIN
+                prod_date_tbl AS prod
+            ON
+                temp.{self.checkpoint_id_col} = prod.{self.checkpoint_id_col}
+
+        )
+        SELECT
+            {column_str}
+        FROM
+            labeled_new_tbl
+        WHERE
+            is_new = 1
         ;
         """
