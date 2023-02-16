23e0c41738d1e74124ce8bfa8230a2c2046193b7
Saul Lee <saul.s.lee@gmail.com>
2023-02-16 14:38:28 -0500

    add code snippet to generate fernet key

---
+++ airflow/generate_fernet_key.py
@@ -0,0 +1,7 @@
+# adapted from
+# https://airflow.apache.org/docs/apache-airflow/stable/administration-and-deployment/security/secrets/fernet.html
+
+from cryptography.fernet import Fernet
+
+fernet_key = Fernet.generate_key()
+print(fernet_key.decode())  # your fernet_key, keep it in secured place!
