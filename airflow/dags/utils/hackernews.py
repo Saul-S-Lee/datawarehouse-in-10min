788d296b745790cedfb2f644cac2cdff225d2e25
Saul Lee <saul.s.lee@gmail.com>
2023-04-28 09:34:05 -0400

    change logic to break out each store id into its own row

--- airflow/dags/utils/hackernews.py
+++ airflow/dags/utils/hackernews.py
@@ -14,6 +14,21 @@ processed_column_top_stories = []
 column_top_stories = source_column_top_stories + processed_column_top_stories
 
 
+def parse_top_stories(top_stories_list, date_str):
+    import uuid
+
+    # generate a random unique for this entry
+    event_id = uuid.uuid4()
+
+    data_list = []
+    for cur_i, cur_story_id in enumerate(top_stories_list):
+        data_list.append(
+            (date_str, event_id, str(cur_story_id), cur_i+1)
+        )
+
+    return data_list
+
+
 class hacker_news_top_stories_table(postgres_table):
     """
     Table for hacker news top stories
